85f1da4c83b2598459cb462f8885493b8e2075fe
Eric <eric.m.lombardo@gmail.com>
2021-04-11 16:02:00 -0400

    Build blog domain model with associations/forms

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
+  has_many :post_tags
+  has_many :tags, through: :post_tags
   
+  validates :name, :content, presence: true
+
+  def tag_ids=(ids)
+    ids.delete("")
+    ids.each do |id|
+      self.tags << Tag.find_by_id(id)
+    end
+  end
+
 end
